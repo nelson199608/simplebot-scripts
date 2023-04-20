ecafbb04d42a896510dfb035cba17834bc0e9d54
nelson199608 <130792780+nelson199608@users.noreply.github.com>
2023-04-19 21:54:11 -0400

    Update bing.py

--- scripts/bing.py
+++ scripts/bing.py
@@ -6,25 +6,24 @@ from simplebot.bot import DeltaBot, Replies
 from bs4 import BeautifulSoup
 
 
-@simplebot.command("/bing")
-def search_command(bot: DeltaBot, message: Message, replies: Replies) -> None:
-    query = message.text.strip()[len("/bing"):].strip()
-    if query:
-        image_urls = _search_images(query)
-        if len(image_urls) == 0:
-            replies.add(text="No se encontraron imágenes para la búsqueda: " + query)
-            return
-        random.shuffle(image_urls)
-        for i in range(min(5, len(image_urls))):
-            image_url = image_urls[i]
-            image_data = requests.get(image_url).content
-            replies.add(image=image_data, quote=message)
+@simplebot.command()
+def bing(bot: DeltaBot, message: Message, replies: Replies) -> None:
+    """Buscar imágenes en bing."""
+    query = message.text
+    image_urls = _search_images(query)
+    if len(image_urls) == 0:
+        replies.add(text="No se encontraron imágenes para la búsqueda: " + query)
+        return
+    random.shuffle(image_urls)
+    for i in range(min(5, len(image_urls))):
+        image_url = image_urls[i]
+        image_data = requests.get(image_url).content
+        replies.add(image=image_data, quote=message)
 
 
 def _search_images(query: str) -> list:
     image_urls = []
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     search_url = "https://www.bing.com/images/search?q=" + query
     response = requests.get(search_url, headers=headers)
     if response.status_code == 200:
