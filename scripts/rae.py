895985ee5cb5cc8c2091408f1ce60a3c081fcaee
nelson199608 <130792780+nelson199608@users.noreply.github.com>
2023-04-20 02:15:54 -0400

    Update rae.py

--- scripts/rae.py
+++ scripts/rae.py
@@ -5,10 +5,14 @@ from deltachat import Message
 from simplebot.bot import DeltaBot, Replies
 from bs4 import BeautifulSoup
 
+import requests
+from bs4 import BeautifulSoup
+from simplebot_instanview import show
+
 @simplebot.command()
 def rae(bot: DeltaBot, message: Message, replies: Replies) -> None:
     """Buscar definiciones en la RAE."""
-    palabra = message.text
+    palabra = message.text.replace("/rae ", "")
     search_url = f"https://dle.rae.es/{palabra}"
     response = requests.get(search_url)
     if response.status_code == 200:
@@ -17,8 +21,11 @@ def rae(bot: DeltaBot, message: Message, replies: Replies) -> None:
         if definiciones:
             definiciones_text = [definicion.text.strip() for definicion in definiciones]
             definiciones_text = '\n'.join(definiciones_text)
+            show(f"Definiciones de '{palabra}':\n{definiciones_text}")
             replies.add(text=f"Definiciones de '{palabra}':\n{definiciones_text}", quote=message)
         else:
+            show(f"No se encontraron definiciones para '{palabra}'.")
             replies.add(text=f"No se encontraron definiciones para '{palabra}'.", quote=message)
     else:
+        show(f"Error al buscar definiciones en la RAE para la palabra: {palabra}")
         replies.add(text=f"Error al buscar definiciones en la RAE para la palabra: {palabra}", quote=message)
