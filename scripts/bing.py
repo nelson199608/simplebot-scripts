d601d4cee2badea37719312048b2836350770f09
nelson199608 <130792780+nelson199608@users.noreply.github.com>
2023-04-19 23:35:42 -0400

    Create bing.py

--- scripts/bing.py
+++ scripts/bing.py
@@ -5,31 +5,20 @@ from deltachat import Message
 from simplebot.bot import DeltaBot, Replies
 from bs4 import BeautifulSoup
 
-
 @simplebot.command()
-def bing(bot: DeltaBot, message: Message, replies: Replies) -> None:
-    """Buscar imágenes en bing."""
+def yandex(bot: DeltaBot, message: Message, replies: Replies) -> None:
+    """Buscar imágenes en Yandex."""
     query = message.text
-    image_urls = _search_images(query)
-    if len(image_urls) == 0:
-        replies.add(text="No se encontraron imágenes para la búsqueda: " + query)
-        return
-    random.shuffle(image_urls)
-    for i in range(min(5, len(image_urls))):
-        image_url = image_urls[i]
-        image_data = requests.get(image_url).content
-        replies.add(image=image_data, quote=message)
-
-
-def _search_images(query: str) -> list:
-    image_urls = []
+    search_url = "https://yandex.com/images/search/"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-    search_url = "https://www.bing.com/images/search?q=" + query
-    response = requests.get(search_url, headers=headers)
+    params = {'text': query}
+    response = requests.get(search_url, headers=headers, params=params)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        image_divs = soup.find_all('div', {'class': 'imgpt'})
-        for div in image_divs:
-            image_url = div.find('a')['href']
-            image_urls.append(image_url)
-    return image_urls
+        image_tags = soup.find_all('img', class_='serp-item__thumb')
+        for i in range(min(len(image_tags), 5)):
+            image_url = image_tags[i]['src']
+            image_data = requests.get(image_url).content
+            replies.add(image=image_data, quote=message)
+    else:
+        replies.add(text="Error al buscar imágenes en Yandex para la búsqueda: " + query)
