50604054bb8a0aecef209323ab684e9bcf21b390
nelson199608 <130792780+nelson199608@users.noreply.github.com>
2023-04-20 01:56:41 -0400

    Update and rename bing.py to rae.py

---
+++ scripts/rae.py
@@ -0,0 +1,24 @@
+import requests
+import random
+import simplebot
+from deltachat import Message
+from simplebot.bot import DeltaBot, Replies
+from bs4 import BeautifulSoup
+
+@simplebot.command()
+def rae(bot: DeltaBot, message: Message, replies: Replies) -> None:
+    """Buscar definiciones en la RAE."""
+    palabra = message.text
+    search_url = f"https://dle.rae.es/{palabra}"
+    response = requests.get(search_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        definiciones = soup.find_all(class_='j')
+        if definiciones:
+            definiciones_text = [definicion.text.strip() for definicion in definiciones]
+            definiciones_text = '\n'.join(definiciones_text)
+            replies.add(text=f"Definiciones de '{palabra}':\n{definiciones_text}", quote=message)
+        else:
+            replies.add(text=f"No se encontraron definiciones para '{palabra}'.", quote=message)
+    else:
+        replies.add(text=f"Error al buscar definiciones en la RAE para la palabra: {palabra}", quote=message)
